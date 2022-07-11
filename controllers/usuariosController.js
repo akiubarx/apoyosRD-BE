e9b5c7e95e8ac30f138cccfa25103d4e161d0cf6
[Jorgejalvarez] <[jorge.alvarez@conafor.gob.mx]>
2022-07-11 09:38:12 -0500

    Ajustes

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,5 +1,6 @@
 import Usuario from '../models/Usuarios.js';
 import generarTokenM from '../helpers/generarTokenManual.js'
+import { isExistUSrMail } from '../customMiddelwares/validaciones.js'
 
 //Consulta Global de Usuarios
 const consultarUsuarios = async (req,res) => {
@@ -9,58 +10,38 @@ const consultarUsuarios = async (req,res) => {
 
 //Creacion de Usuario
 const crearUsuario = async (req, res) => {
-  const { email, username } = req.body;
 
-  const existeUsuarioCorreo = await Usuario.findOne({email}); //Busca si existe un registro que coincida
-  const existeUsuario = await Usuario.findOne({username}); //Busca si existe un registro que coincida
-  const existeId = await Usuario.find({}).sort([['id',-1]]).limit(1); //Busca el ultimo registro
-  let newId = parseInt(existeId[0].id);
-  if(newId) newId++;//Aumenta el ultimo registro en 1 */
+  if(isExistUSrMail({req,res})){
+    const existeId = await Usuario.find({}).sort([['id',-1]]).limit(1); //Busca el ultimo registro
+    let newId = parseInt(existeId[0].id);
+    if(newId) newId++;//Aumenta el ultimo registro en 1
 
-  if(existeUsuarioCorreo){
-    const error = new Error('Correo ya registrado'); 
-    return res.status(400).json({msg: error.message});
-  }
-  if(existeUsuario){
-    const error = new Error('Nombre de Usuario ya registrado');
-    return res.status(400).json({msg: error.message});
-  }
-  try {
-    const usuario = new Usuario({...req.body,id:newId});//añade un nuevo ID en caso de ser necesario
-    usuario.token = generarTokenM();
-    const usuarioAlmacenado = await usuario.save();
-    res.json(usuarioAlmacenado);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+      const usuario = new Usuario({...req.body,id:newId});//añade un nuevo ID en caso de ser necesario
+      usuario.token = generarTokenM();
+      const usuarioAlmacenado = await usuario.save();
+      res.json(usuarioAlmacenado);
+    } catch (error) {
+      console.log('Error al registrar Usuario');
+    }
+  };
 }
 
 //Edicion de Usuario
 const editarUsuario = async (req,res) => {
-  const { id, email, username } = req.body;
-  const usuario = await Usuario.findOne( {id} ); //find() el ID que coincida
+  const { id } = req.body;
+  if(isExistUSrMail({req,res})){
+    const usuario = await Usuario.findOne( {id} ); //find() el ID que coincida
+    usuario.username = req.body.username || usuario.username; 
+    usuario.email = req.body.email || usuario.email;
 
-  const existeUsuarioCorreo = await Usuario.findOne({email}); //Busca si existe un registro que coincida
-  const existeUsuario = await Usuario.findOne({username}); //Busca si existe un registro que coincida
-  
-  usuario.username = req.body.username || usuario.username; 
-  usuario.email = req.body.email || usuario.email;
-
-  if(existeUsuarioCorreo){
-    const error = new Error('Correo ya registrado'); 
-    return res.status(400).json({msg: error.message});
-  }
-  if(existeUsuario){
-    const error = new Error('Nombre de Usuario ya registrado');
-    return res.status(400).json({msg: error.message});
-  }
-
-  try {
-    const usuarioModificado = await usuario.save()
-    res.json(usuarioModificado);
-  } catch (error) {
-    console.log(error)
-  }
+    try {
+      const usuarioModificado = await usuario.save()
+      res.json(usuarioModificado);
+    } catch (error) {
+      console.log('Error al editar Usuario');
+    }
+  };
 }
 
 // Exports del controlador
---
+++ customMiddelwares/validaciones.js
@@ -0,0 +1,18 @@
+import Usuario from '../models/Usuarios.js';
+
+const isExistUSrMail = async (props) => {
+  let { req, res } = props;
+  let { email, username } = req.body;
+
+  const existeUsuarioCorreo = await Usuario.findOne({email}); //Busca si existe un registro que coincida
+  const existeUsuario = await Usuario.findOne({username}); //Busca si existe un registro que coincida
+
+  if(existeUsuarioCorreo || existeUsuario){
+    const error = new Error('Correo o Usuario ya registrado'); 
+    return res.status(400).json({msg: error.message});
+  }
+}
+
+export {
+  isExistUSrMail
+}
